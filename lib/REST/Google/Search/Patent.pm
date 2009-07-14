26cb3b1e89ac8fbfe13f870308b6088340d8523b
Eugen Sobchenko <ejs@cpan.org>
2009-07-14 12:56:15 +0000

    Import of EJS/REST-Google-1.0.6 from CPAN.
    gitpan-cpan-distribution: REST-Google
    gitpan-cpan-version:      1.0.6
    gitpan-cpan-path:         EJS/REST-Google-1.0.6.tar.gz
    gitpan-cpan-author:       EJS
    gitpan-cpan-maturity:     released

--- lib/REST/Google/Search/Patent.pm
+++ lib/REST/Google/Search/Patent.pm
@@ -1,7 +1,7 @@
 #
-# $Id: Web.pm 10 2008-04-29 21:31:10Z esobchenko $
+# $Id$
 
-package REST::Google::Search::Web;
+package REST::Google::Search::Patent;
 
 use strict;
 use warnings;
@@ -11,6 +11,6 @@ use version; our $VERSION = qv('1.0.6');
 require REST::Google::Search;
 use base qw/REST::Google::Search/;
 
-__PACKAGE__->service( &REST::Google::Search::WEB );
+__PACKAGE__->service( &REST::Google::Search::PATENT );
 
 return 1;
